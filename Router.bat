39e0674bb04e62e72f43c109b5cbad9fae6f5736
Alfred Loo <30677781+alfredloo@users.noreply.github.com>
2020-12-26 12:40:52 +0800

    Create Router.bat

---
+++ Router.bat
@@ -0,0 +1,13 @@
+#Router
+show clock
+show version
+show running 
+show redundancy
+show license all
+show ip route
+show logging onboard router active uptime detail
+show cdp neighbor
+show inventory
+show platform
+show environment all
+show clock
